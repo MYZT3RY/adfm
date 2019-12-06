9d06bb92f2bc16ac8e67e7e5e2790419c7b6ad64
MYZT3RY <d1maz.d@yandex.ru>
2019-12-07 00:44:19 +0300

    added ListView with folders&apps, change directory, run apps

--- Another Damn File Manager/Form1.Designer.cs
+++ Another Damn File Manager/Form1.Designer.cs
@@ -28,11 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
             this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.aboutProgramToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.currentDirectoryTextBox = new System.Windows.Forms.TextBox();
+            this.arrayLeftButton = new System.Windows.Forms.Button();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -43,7 +49,7 @@
             this.helpToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(877, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -58,7 +64,7 @@
             // toolStripMenuItem3
             // 
             this.toolStripMenuItem3.Name = "toolStripMenuItem3";
-            this.toolStripMenuItem3.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(109, 22);
             this.toolStripMenuItem3.Text = "Выйти";
             this.toolStripMenuItem3.Click += new System.EventHandler(this.toolStripMenuItem3_Click);
             // 
@@ -73,18 +79,67 @@
             // aboutProgramToolStripMenuItem
             // 
             this.aboutProgramToolStripMenuItem.Name = "aboutProgramToolStripMenuItem";
-            this.aboutProgramToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.aboutProgramToolStripMenuItem.Size = new System.Drawing.Size(149, 22);
             this.aboutProgramToolStripMenuItem.Text = "О программе";
             this.aboutProgramToolStripMenuItem.Click += new System.EventHandler(this.aboutProgramToolStripMenuItem_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.LargeImageList = this.imageList1;
+            this.listView1.Location = new System.Drawing.Point(0, 65);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(877, 441);
+            this.listView1.SmallImageList = this.imageList1;
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.ItemSelectionChanged += new System.Windows.Forms.ListViewItemSelectionChangedEventHandler(this.listView1_ItemSelectionChanged);
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            this.listView1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseDoubleClick);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "arrayLeft.png");
+            this.imageList1.Images.SetKeyName(1, "emptyFolder.png");
+            this.imageList1.Images.SetKeyName(2, "application.png");
+            // 
+            // currentDirectoryTextBox
+            // 
+            this.currentDirectoryTextBox.Location = new System.Drawing.Point(194, 36);
+            this.currentDirectoryTextBox.Name = "currentDirectoryTextBox";
+            this.currentDirectoryTextBox.Size = new System.Drawing.Size(671, 20);
+            this.currentDirectoryTextBox.TabIndex = 2;
+            this.currentDirectoryTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.currentDirectoryTextBox_KeyDown);
+            // 
+            // arrayLeftButton
+            // 
+            this.arrayLeftButton.ImageKey = "arrayLeft.png";
+            this.arrayLeftButton.ImageList = this.imageList1;
+            this.arrayLeftButton.Location = new System.Drawing.Point(12, 30);
+            this.arrayLeftButton.Name = "arrayLeftButton";
+            this.arrayLeftButton.Size = new System.Drawing.Size(34, 30);
+            this.arrayLeftButton.TabIndex = 3;
+            this.arrayLeftButton.UseVisualStyleBackColor = true;
+            this.arrayLeftButton.Click += new System.EventHandler(this.arrayLeftButton_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(877, 506);
+            this.Controls.Add(this.arrayLeftButton);
+            this.Controls.Add(this.currentDirectoryTextBox);
+            this.Controls.Add(this.listView1);
             this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Another Damn File Manager";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
@@ -99,6 +154,10 @@
         private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
         private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem aboutProgramToolStripMenuItem;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.TextBox currentDirectoryTextBox;
+        private System.Windows.Forms.Button arrayLeftButton;
     }
 }
 
--- Another Damn File Manager/Form1.cs
+++ Another Damn File Manager/Form1.cs
@@ -10,6 +10,8 @@ using System.Windows.Forms;
 
 namespace Another_Damn_File_Manager{
     public partial class Form1 : Form{
+
+        private string path;
         public Form1(){
             InitializeComponent();
         }
@@ -19,7 +21,55 @@ namespace Another_Damn_File_Manager{
         }
 
         private void aboutProgramToolStripMenuItem_Click(object sender, EventArgs e){
-            MessageBox.Show("Another Damn File Manager - Ещё один чёртов файловый менеджер.\n\nПроизведено ручками d1maz. - Дмитрий Якимов.\nДля свободного использования.\n\nРаспространяется по лицензии Apache 2.0\n\nd1maz.ru/adfm\ngithub.com/MYZT3RY/adfm\n\n2019 - 2019", "О прорамме");
+            MessageBox.Show("Another Damn File Manager - Ещё один чёртов файловый менеджер.\n\nПроизведено ручками d1maz. - Дмитрий Якимов.\nДля свободного использования.\n\nРаспространяется по лицензии GPL-3.0\n\nd1maz.ru/adfm\ngithub.com/MYZT3RY/adfm\n\n2019 - 2019", "О прорамме");
+        }
+
+        private void Form1_Load(object sender, EventArgs e){
+            initListView(System.IO.Directory.GetCurrentDirectory(), listView1, imageList1);
+        }
+
+        private void initListView(string path, ListView listView, ImageList imageList){
+            listView.Clear();
+            currentDirectoryTextBox.Text = path;
+            foreach (var element in System.IO.Directory.GetDirectories(path)){
+                listView.Items.Add(element.Substring(element.LastIndexOf("\\") + 1), imageList.Images.Count - 2);
+            }
+            foreach (var element in System.IO.Directory.GetFiles(path)){
+                listView.Items.Add(element.Substring(element.LastIndexOf("\\") + 1), imageList.Images.Count - 1);
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e){
+            
+        }
+
+        private void listView1_ItemSelectionChanged(object sender, ListViewItemSelectionChangedEventArgs e){
+            path = System.IO.Directory.GetCurrentDirectory() + "\\" + e.Item.Text;
+        }
+
+        private void listView1_MouseDoubleClick(object sender, MouseEventArgs e){
+            if (System.IO.Directory.Exists(path)){
+                System.IO.Directory.SetCurrentDirectory(path);
+                initListView(path, listView1, imageList1);
+            }
+            else{
+                System.Diagnostics.Process.Start(path);
+            }
+        }
+
+        private void currentDirectoryTextBox_KeyDown(object sender, KeyEventArgs e){
+            if(e.KeyCode == Keys.Enter){
+                string tmpPath;
+                tmpPath = currentDirectoryTextBox.Text;
+                System.IO.Directory.SetCurrentDirectory(tmpPath);
+                initListView(path, listView1, imageList1);
+            }
+        }
+
+        private void arrayLeftButton_Click(object sender, EventArgs e){
+            var tmpPath = System.IO.Directory.GetParent(System.IO.Directory.GetCurrentDirectory());
+            System.IO.Directory.SetCurrentDirectory(tmpPath.ToString());
+            initListView(tmpPath.ToString(), listView1, imageList1);
         }
     }
 }
